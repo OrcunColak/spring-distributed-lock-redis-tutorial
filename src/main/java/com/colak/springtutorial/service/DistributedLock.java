6dfe162d9c5dcb53a5ba64c4020f1b12ea66e5b1
Orçun Çolak <orcun.colak@hazelcast.com>
2024-09-19 13:51:29 +0300

    enhance release logic

--- src/main/java/com/colak/springtutorial/service/DistributedLock.java
+++ src/main/java/com/colak/springtutorial/service/DistributedLock.java
@@ -12,14 +12,28 @@ public class DistributedLock {
 
     private final RedisTemplate<String, String> redisTemplate;
 
-    public boolean acquire(String lockName, long timeout, TimeUnit unit) {
-        String key = "lock:" + lockName;
-        return Boolean.TRUE.equals(redisTemplate.opsForValue()
-                .setIfAbsent(key, "locked", timeout, unit));
+    public boolean acquire(String lockValue, long timeout, TimeUnit unit) {
+        String lockKey = getLockKey(lockValue);
+        return redisTemplate.opsForValue()
+                .setIfAbsent(lockKey, "locked", timeout, unit);
     }
 
-    public boolean release(String lockName) {
-        String key = "lock:" + lockName;
-        return Boolean.TRUE.equals(redisTemplate.delete(key));
+    public boolean release(String lockValue) {
+        String lockKey = getLockKey(lockValue);
+        String currentValue = redisTemplate.opsForValue().get(lockKey);
+
+        // See https://medium.com/@anil.goyal0057/distributed-locking-mechanism-using-redis-26c17d9f3d5f
+        // This is important to avoid removing a lock that was created by another client.
+        // For example, a client may acquire the lock, get blocked performing some operation for longer than the lock
+        // validity time (the time at which the key will expire), and later remove the lock, that was already acquired
+        // by some other client.
+        if (currentValue != null && currentValue.equals(lockValue)) {
+            return redisTemplate.delete(lockKey);
+        }
+        return false;
+    }
+
+    private static String getLockKey(String lockValue) {
+        return "lock:" + lockValue;
     }
 }
